ec7d029bef1b0f872c1b9f4bb47a4deb58968349
Yemi Ogundairo <62849248+SodiqOgundairo@users.noreply.github.com>
2025-08-25 15:41:04 +0100

    UI components updated

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
         <Route path="/trainings" element={<TrainingsPage />} />
         <Route path="/contact" element={<ContactPage />} />
       </Routes>
-      <Footer />
+      <Footer /> 
     </Router>
   );
 }
