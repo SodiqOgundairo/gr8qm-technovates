bf6f5d6931a89050bf05c6f46738a9cd9199b31a
Sodiqogundairo <kingmclamba1@gmail.com>
2025-08-29 20:46:51 +0100

    SEO

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,6 +6,11 @@ import Contact from '../components/Contact';
 const Home: React.FC = () => {
   return (
     <div>
+       <metadata>
+        <title>Gr8QM Technovates | Home </title>
+        <meta name="description" content="Gr8QM Home" />
+        <meta property="og:title" content="Gr8QM Technovates" />
+      </metadata>
       <Hero />
       <Services />
       <About />
