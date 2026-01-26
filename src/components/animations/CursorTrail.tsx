d637423a11aaa1f025bcf8d939256689c6647e59
Yemi Ogundairo <62849248+SodiqOgundairo@users.noreply.github.com>
2026-01-26 19:26:21 +0100

    feat: ripple effect animations

--- src/components/animations/CursorTrail.tsx
+++ src/components/animations/CursorTrail.tsx
@@ -18,7 +18,7 @@ const CursorTrail: React.FC = () => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const particlesRef = useRef<Particle[]>([]);
   const mouseRef = useRef({ x: 0, y: 0 });
-  const animationFrameRef = useRef<number>();
+  const animationFrameRef = useRef<number | undefined>(undefined);
 
   useEffect(() => {
     const canvas = canvasRef.current;
